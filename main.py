d705f15a0f7677f1b6513c5caa667869d4fc4cfc
Tyujhty <fannybaquey@gmail.com>
2024-01-14 13:06:47 +0100

    feat: add while loop to stop the race. Add xcor() function to test which turtle touch the edge of screen.

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
+import turtle
 from turtle import Turtle, Screen
+import random
 
 screen = Screen()
 screen.setup(width=500, height=400)
-
 colors = ["red", "orange", "yellow", "blue", "purple", "green"]
 '''Initialize the user bet'''
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle is going to win? Enter a color: ")
 
 y_position = -125
+all_turtles = []
 
 for turtle_index in range(0, 6):
-    tony = Turtle(shape="turtle")
-    tony.color(colors[turtle_index])
-    tony.penup()
-    tony.goto(x=-240, y=y_position)
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-240, y=y_position)
     y_position += 50
+    all_turtles.append(new_turtle)
 
+if user_bet:
+    is_race_on = True
 
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won ! The  {winning_color} is the winner")
+            else:
+                print(f"You've lost ! The  {winning_color} is the winner")
+        random_forward = random.randint(0, 15)
+        turtle.forward(random_forward)
 
 screen.exitonclick()
